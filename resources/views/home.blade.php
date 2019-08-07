29354a1a0aef9aff62cb05495fa101127fbfb673
fitrinanur <missfitrina@gmail.com>
2019-08-07 17:45:27 +0700

    change home2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,3 +27,90 @@
     </div>
 </div>
 @endsection
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8">
+                <section id="slide" style="max-width:1000px; max-height:400px !important;">
+                    <div class="bd-example">
+                        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="{{ asset('lalin.jpg') }}" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>First slide label</h5>
+                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('lalin0.jpg') }}" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Second slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('lalin2.jpg') }}" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Third slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('ujiemisi.jpg') }}" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Fourth slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('mudik1.jpg') }}" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>Fifth slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('mudik2.jpg') }}" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5>six slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"
+                                data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button"
+                                data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <p>Selamat Datang , {{ Auth()->user()->name }}</p>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas tenetur dignissimos illo. Facere sint officia perspiciatis debitis maiores quos similique, quam explicabo soluta adipisci eius, veritatis optio facilis dolore accusamus sit sequi tempore ipsam eos deleniti distinctio pariatur! Iusto non harum quia, fugit veniam quisquam amet voluptas quos adipisci! Cum eaque odit vero nemo doloribus dolorem a ut modi molestiae, laudantium libero praesentium alias voluptas optio corporis perferendis nulla quam fuga atque repudiandae? Repellat tempora ad deserunt enim sit delectus nostrum maiores architecto natus ab atque, nihil perspiciatis adipisci possimus repellendus, reiciendis est rerum voluptatibus ipsum corrupti. Voluptates sed a et repudiandae placeat cum voluptatum tempore cupiditate eligendi nulla, necessitatibus ipsam eaque tempora labore natus nesciunt minus laudantium, qui deserunt! Possimus porro necessitatibus ab cumque, dolorum provident est cum. Adipisci laudantium odio voluptatem, ad, minus nulla, possimus nostrum recusandae natus enim error minima quaerat illum quasi corporis nam nesciunt non!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,91 +15,259 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Acme|Montserrat:800|Source+Sans+Pro:400,700&display=swap"
+        rel="stylesheet">
 
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <style>
-    body {
-        background-image: url("{{asset('simple-pattern.png')}}");
-        backgound-repeat: repeat-x repeat-y;
-    }
+        body {
+            /* background-image: url("{{ asset('simple-pattern.jpeg') }}") ; */
+            background-repeat: repeat-x repeat-y;
+        }
+
+        .top-header {
+            background-color: #3F1871;
+            font-family: 'Source Sans Pro', sans-serif;
+        }
+
+        .top-right {
+            float: right;
+        }
+
+        .top-nav {
+            float: left;
+        }
+
+        .top-title-nav>a {
+            font-family: 'Source Sans Pro', sans-serif;
+            color: white;
+            font-weight: 700;
+            font-size: 20px;
+        }
+
+        .header-info>ul {
+            list-style-type: none;
+            display: inline-flex;
+            padding: 5px;
+        }
+
+        .header-info>ul>li {
+            padding: 2px 10px;
+            margin-left: 10px;
+        }
+
+        .header-info>ul>li>a {
+            color: white;
+            font-size: 25px;
+        }
+
+        .navbar {
+            padding: 13px !important;
+        }
+
+        .navbar-collapse {
+            padding: 10px !important;
+        }
+
+        .navbar-collapse>ul>li>a {
+            font-family: 'Source Sans Pro', sans-serif;
+            color: #888888;
+            font-weight: 600;
+            font-size: 18px !important;
+            line-height: 23px !important;
+            text-transform: capitalize !important;
+            padding: 7px !important;
+            margin: 10px !important;
+        }
+
+        footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #3F1871;
+            color: white;
+            text-align: center;
+            font-size: 14px;
+            padding: 10px;
+        }
+
+    
+
+        footer > .phone_number {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 14px;
+        }
+
+        .copyright > p {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 16px;
+        }
+        footer > .phone_number > p, a {
+            color: white;
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 16px;
+        }
+
+        footer .number_pengaduan {
+            color: white;
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 16px;
+        }
+
     </style>
 </head>
+
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{-- {{ config('app.name', 'Laravel') }} --}}
-                Kmeans
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home')}}">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Data Master
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('road.index')}}">Data Jalan</a>
-                            <a class="dropdown-item" href="{{ route('user.index')}}">Data User</a>
+    <div id="app">
+        <header id="header" class="site-header">
+            <div class="top-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <nav class="top-nav">
+                                <div class="top-title-nav">
+                                    <a href="http://www.perhubungan.jatengprov.go.id/webmail"
+                                        target="_blank">Webmail</a>
+                                </div>
+                            </nav>
                         </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('proses.index')}}">Proses</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('maps.index')}}">Maps</a>
-                    </li>
-                </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
+                        <div class="col-md-6">
+                            <div class="top-right">
+                                <div class="header-info">
+                                    <ul class="header-social">
+                                        <li><a href="https://www.facebook.com/perhubunganjtg/" title="facebook"
+                                                target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li><a href="https://twitter.com/perhubunganjtg" title="Twitter"
+                                                target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li><a href="https://www.instagram.com/dinhub.jateng/" title="Instagram"
+                                                target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                        </li>
+                                        <li><a href="https://www.youtube.com/channel/UCe4WuIvGOGGfjhfCZ3jYX_Q"
+                                                title="Youtube" target="_blank"><i class="fa fa-youtube"
+                                                    aria-hidden="true"></i></a></li>
+                                    </ul>
+                                </div><!-- .header-info -->
+                            </div><!-- .top-right -->
+                        </div>
+                    </div><!-- .row -->
+                </div><!-- .container -->
+            </div><!-- .top-header -->
+        </header>
+        <nav class="navbar navbar-expand-md shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}" style="color:#888888;font-family: 'Source Sans Pro', sans-serif;font-weight:600; font-size: 18px !important;
+                line-height: 23px !important;">
+                    {{-- {{ config('app.name', 'Laravel') }} --}}
+                    Kmeans
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home')}}">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Data Master
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('road.index')}}">Data Jalan</a>
+                                <a class="dropdown-item" href="{{ route('user.index')}}">Data User</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('proses.index')}}">Proses</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('maps.index')}}">Maps</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
-                        
-                    @else
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
                                     @csrf
                                 </form>
                             </div>
                         </li>
-                    @endguest
-                </ul>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <main class="py-4">
-        @yield('content')
+        <main class="py-4">
+            @yield('content')
 
-    </main>
-</div>
-@stack('scripts');
+        </main>
+        <footer id="footer" class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="copyright">
+                            <p>Copyright © 2017 Dinas Perhubungan Provinsi Jawa Tengah</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <span class="pull-right number_pengaduan">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <p>Pengaduan 
+                                    </p>
+                                </div>
+                                <div class="col-lg-8 phone_number" >
+                                    <p>
+                                        <i class="fa fa-whatsapp" aria-hidden="true"></i> &nbsp;
+                                         <a href="https://wa.me/628112790123">0811-279-0123</a> 
+                                    </p>      
+                                </div>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    @stack('scripts');
 </body>
+
 </html>
